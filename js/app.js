98303ca6a6e73c6474bf091eee11141d1df12f71
Matt Mobley <mattsmobley@gmail.com>
2016-08-04 15:25:15 -0600

    adding some new animations

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+$( ".aboutButton" ).click(function() {
+  $( ".aboutApp" ).animate({
+    width: "30%",
+    opacity: 1,
+    fontSize: "120%",
+    paddingTop: "5%",
+    marginLeft: "1%"
+  }, 1000 );
+});
+
+$( ".aboutButton" ).click(function(){
+  $(".videoPadding").animate({
+    paddingLeft: "2%",
+    paddingRight: "2%"
+  })
+})
+
+$( ".exit" ).click(function(){
+  $( ".aboutApp" ).animate({
+    width: "0%",
+    opacity: 0,
+    fontSize: "0em"
+  }, 1000 );
+});
+
+$(".exit").click(function() {
+  $('.videoPadding').animate({
+    paddingLeft: "10%",
+    paddingRight: "10%"
+  }, 1000);
+});
